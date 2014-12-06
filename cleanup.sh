1750ef5560e85e5d792986fa90cc9b50a86e4f17
Thomas Pöchtrager <t.poechtrager@gmail.com>
2014-12-06 12:08:01 +0100

    simplify cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,24 +1,2 @@
 #!/bin/sh
-rm -f config.status
-rm -f configure
-rm -f *stamp*
-rm -f install-sh
-rm -f depcomp
-rm -f Makefile.in
-rm -f Makefile
-rm -f missing
-rm -f aclocal.m4
-rm -f -rf autom4te.cache
-rm -f config.h
-rm -f *.log
-rm -f *.scan
-rm -f src/*.save
-rm -f src/*.o
-rm -f src/Makefile.in
-rm -f src/Makefile
-rm -f -rf src/.deps
-rm -f *.*~
-rm -f src/*.*~
-rm -f -rf src/obj
-rm -f -rf bin
-rm -f src/wclang
+git clean -fdx 1>/dev/null
